2d485dbb371534251ac8f2ce3d1bde6284006f15
DominicSherman <dominic.sherman98@gmail.com>
2019-04-19 18:04:12 -0500

    Lints

--- client/src/components/TwitterAnalytics.js
+++ client/src/components/TwitterAnalytics.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 
 import FollowerRatio from '../graphs/FollowerRatio';
 import {TWITTER} from '../constants/analytic-types';
-import FriendsProgressBar from "../graphs/FriendsProgressBar";
-import styles from "../css/components/PageLayout.module.css"
+import FriendsProgressBar from '../graphs/FriendsProgressBar';
+import styles from '../css/components/PageLayout.module.css';
 
 export default class TwitterAnalytics extends Component {
     render() {
@@ -15,7 +15,7 @@ export default class TwitterAnalytics extends Component {
                 />
                 <div className={styles.graphWrapper}>
                     <FriendsProgressBar
-                        platform = {TWITTER}
+                        platform={TWITTER}
                         {...this.props}
                     />
                 </div>
